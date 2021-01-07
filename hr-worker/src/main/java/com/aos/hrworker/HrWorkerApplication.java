cd09e546a000e88a290edd90e127ec17105529dd
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2021-01-06 23:42:48 -0300

    Eureka client config

--- hr-worker/src/main/java/com/aos/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/aos/hrworker/HrWorkerApplication.java
@@ -2,8 +2,9 @@ package com.aos.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
